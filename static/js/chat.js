8618dd15e74d543b571bfc1d4fd9de0e4080552e
a-fayez <ahmedf.higgy@gmail.com>
2020-04-19 17:15:16 +0200

    some js functionality for pooup

--- static/js/chat.js
+++ static/js/chat.js
@@ -3,15 +3,23 @@
     load the chat conten
 */
 document.addEventListener('DOMContentLoaded', () => {
+
     const displayName = document.querySelector("#name").innerHTML;
     localStorage.setItem('name', displayName);
 
+    // control displaying of popup
+    const popup = document.querySelector(".popup");
     document.querySelector("#new-channel-btn").onclick = () => {
+        popup.style.display = "flex";
+    };
+    document.querySelector("#cancel").onclick = () => {
+        popup.style.display = "none";
+    };
 
-        const windowParam = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
-                             width=50%,height=50%,left=25%,top=25%`;
+    // creating new channel
+    document.querySelector("#create").onclick = () => {
+        // TODO:  
+    };
 
-        window.open("/popup", "New Channel",  windowParam).focus();
-    }
 
 });
