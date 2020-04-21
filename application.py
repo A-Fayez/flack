45b25d0868a34a56b7b03886ff90d87ae449b894
a-fayez <ahmedf.higgy@gmail.com>
2020-04-21 10:44:16 +0200

    improved routing

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, jsonify
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -8,6 +8,8 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 users = []
+channels = []
+messages = {} # K: <string>, V: <dict, k:<string(user)>, v:<list of string messages>
 
 @app.route("/")
 def index():
@@ -25,15 +27,27 @@ def chat():
         name = request.form.get("name")
         if name.lower() in users: # display name conflicts
             return render_template("display.html", auth="False")
-        
         users.append(name.lower())
         return render_template("chat.html", display_name=name)
+    
+    return 405
 
 
 # TODO: 
-@app.route("/popup", methods=["GET"])
-def popup():
-    return "TEST POPUP"
+@app.route("/channels", methods=["GET", "POST"])
+def channels():
+    """An endpoint that returns a list of previously created channels
+    in case of a GET request. And create a new channel in case of a
+    POST request.
+    """
+    if request.method == "GET":
+        return jsonify({
+                    "channels": channels
+                 })
+
+    elif request.method == "POST":
+        # TODO: get JSON data and append to channels list
+
 
 if __name__ == '__main__':
     socketio.run(app)
--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,3 @@
-/*
-    First check user's local storage if the users has joined before
-    If so, make a get request to get the chat template
-*/
-
-// TODO: history push and modifiy URLS
-
 const displayName = localStorage.getItem('name');
 if (displayName) {
     window.location = "/chat?name=" + displayName;
@@ -15,7 +8,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
-function auth(name) {
-    
-}
 
